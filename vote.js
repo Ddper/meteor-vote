22fd60c7c3c4ae175d18d19b8c1c3ff6dff80831
iyangsg <iyangsg@gmail.com>
2013-08-26 01:21:53 +0800

    更改项目

--- vote.js
+++ vote.js
@@ -1,12 +1,6 @@
 // 参与投票项目
 Projects = new Meteor.Collection("projects");
 
-// 设立的奖项
-Awards = new Meteor.Collection("awards");
-
-// 得分
-Scores = new Meteor.Collection("scores");
-
 // 投票人
 Voters = new Meteor.Collection("voters");
 
@@ -15,8 +9,16 @@ if (Meteor.isClient) {
     return Session.get('voter');
   };
 
+  Template.vote.voted = function () {
+    return Session.get("voted");
+  };
+
   Template.vote.projects = function () {
     return Projects.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.vote.voters = function () {
+    return Voters.find({}, {sort: {created: -1, name: 1}});
   };
 
   Template.vote.selected_name = function () {
@@ -30,32 +32,46 @@ if (Meteor.isClient) {
 
   Template.vote.events({
     'click input.inc': function () {
-      if (Session.get('voter')) {
-        // 是否已投
+      var name = Session.get("voter");
+      if (name) {
       } else {
         var name = $("input#voter").val().trim();
-        Session.set('voter', name);
+        if (name) {
+          Session.set('voter', name);
+          Session.set('voted', true);
+        } else {
+            // 输入姓名为空
+            return;
+        }
       }
-      Projects.update(Session.get("selected_project"), {$inc: {score: 1}});
-      Projects.update(Session.get("selected_project"), {$inc: {progress: 4}});
+      // 设置分数和进度
+      Projects.update(Session.get("selected_project"), {$inc: {score: 1, progress: 4}});
+      
+      // 增加投票记录
+      project = Projects.findOne(Session.get("selected_project"));
+      Voters.insert({voter: name, project: project.name, created: Date.now()});
     },
   });
 
   Template.project.events({
     'click': function () {
-      if (Session.get('voter')) {
-        //
-      } else {
-        Session.set("selected_project",  this._id);
-      }
+      Session.set("selected_project",  this._id);
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Projects.find().count() === 0) {
-      // 初始化设置队名
+    // Voters.remove({});
+
+    //  换奖项
+    //  Projects.update({}, {$set: {score: 0, progress: 0}}, {multi: true});
+
+    // 换项目
+    var change = false; 
+    if (change) {
+      var names = ["东池", "健康食坊", "一步味道", "秦唐面道", "真功夫", "嘉旺"];
+    } else {
       var names = ["找你位队",
                    "爬虫队",
                    "开店123队",
@@ -69,12 +85,15 @@ if (Meteor.isServer) {
                    "潜伏队",
                    "高端大气上档次",
                    "无名队"];
+    }
 
+    if (Projects.find().count() === 0) {
       for (var i = names.length - 1; i >= 0; i--) {
-        Projects.insert({name: names[i], score: 0, progress: 0});
+        Projects.insert({name: names[i], score: 0, progress: 0, created: Date.now()});
       };
     } else {
       // Projects.remove({});
+      console.log('hanker');
     }
   });
 }
